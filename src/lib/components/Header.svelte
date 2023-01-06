e50a9a6777b0ea7dd086023000a525f83af00560
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-01-05 22:59:44 -0800

    some basic coloring

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <header>
-	<div class = "nav-wrapper-wrapper">
-	<div class="nav-wrapper">
+	<section class = "navigation">
+	<div class="content-container">
 		<a href="/" class="brand">Jeremy Hsieh</a>
 		<nav>
 			<ul>
@@ -10,16 +10,22 @@
 			</ul>
 		</nav>
 	</div>
-	</div>
+	</section>
 </header>
 
 <style>
+	.brand{
+		font-family: var(--font-family-base);
+		font-weight: 700;
+		color:white;
+
+	}
 	nav {
 		padding-top: 10px;
 	}
 	header {
 		padding: 1rem;
-		background: lightskyblue;
+		/* background: lightskyblue; */
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
@@ -27,7 +33,7 @@
 
 	ul {
 		margin: 0;
-		list-style-type: none;
+		list-style-type: none; 
 		display: flex;
 		gap: 1rem;
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 </script>
+
 <div class="my-4">
 	<h1 class="text-center text-3xl font-bold">my cool blog</h1>
 </div>
@@ -8,6 +9,18 @@
 <p class="text-3xl">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<style>
 
+<style>
+	:global(:root) {
+		--content-width: 1160px;
+		--font-family-base: 'Inter', -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Arial,
+			sans-serif;
+		--font-family-monospace: Menlo, 'IBM Plex Mono', Menlo, monospace;
+		--font-family-heading: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Arial, sans-serif;
+		--font-family-nav: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Arial, sans-serif;
+    	--background-color: #1f1f1f;
+		/* --background-color: #333333 */
+		background-color: var(--background-color);
+
+	}
 </style>
