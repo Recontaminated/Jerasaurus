742264e79abb5431a439a64819fd1f2a691c42ac
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-03-31 19:41:52 -0300

    fix: fix prismjs

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,22 +3,20 @@
     import Header from "$lib/components/Header.svelte";
     // import "$lib/styles/blogStyles.scss"
     import {onMount} from "svelte";
+    import Prism from 'prismjs';
     export let data = {
         content: ''
     };
     //call code on the browser
     onMount(() => {
-        // window.Prism.highlightAll();
+        Prism.highlightAll();
     })
 </script>
 
 <svelte:head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-atom-dark.min.css" integrity="sha512-GZPS1oCebjx8g/ZkrTTvWirW+4wDkzIsilUQPXcZzuDpDzoH5brM2AojiFjo6ObIWeM68ZDostvdjaS9MNYCTg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </svelte:head>
-<svelte:body>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-core.min.js" integrity="sha512-9khQRAUBYEJDCDVP2yw3LRUQvjJ0Pjx0EShmaQjcHa6AXiOv6qHQu9lCAIR8O+/D8FtaCoJ2c0Tf9Xo7hYH01Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js" integrity="sha512-SkmBfuA2hqjzEVpmnMt/LINrjop3GKWqsuLSSB3e7iBmYK7JuWw4ldmmxwD9mdm2IRTTi0OxSAfEGvgEi0i2Kw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</svelte:body>
+
 
 <Header />
 <div class="content-container">
