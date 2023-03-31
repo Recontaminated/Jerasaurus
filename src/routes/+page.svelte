e5bf4df750a3cba83a838d6313f1a9fd068ab3ce
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-03-31 18:25:24 -0300

    feat: added preconnect for google fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
 
 </style>
 <svelte:head>
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!--   <link rel="preconnect" href="https://fonts.googleapis.com">-->
+<!--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
     <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet">
 </svelte:head>
