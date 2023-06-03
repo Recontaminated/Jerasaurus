0d5231a5e05328389c9a21e033da236b6fba678f
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-06-02 17:52:09 -0700

    reduce CLS

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,13 +6,23 @@
 
     import { env } from '$env/dynamic/public';
     export let data = {
-        content: ''
+        content: '',
+        json: {
+            data: undefined
+        }
     };
     //call code on the browser
     onMount(() => {
 
         Prism.highlightAll();
     })
+
+    let artAR;
+   // this is so dumb
+    if (data.json.data[0].attributes.blogArt.data != null)
+    {
+       artAR = data.json.data[0].attributes.blogArt.data?.attributes.width / data.json.data[0].attributes.blogArt.data?.attributes.height *100;
+    }
 
 </script>
 
@@ -28,13 +38,31 @@
     <h1 class="blog-title">{data.json.data[0].attributes.title}</h1>
     <p id="pageDate">Updated: {(data.json.data[0].attributes.updatedAt).slice(0,10)} </p>
     {#if data.json.data[0].attributes.blogArt.data != null}
+        <div
+                class="dynamic-aspect-ratio-box"
+                style={`padding-bottom: ${artAR}%`}
+        >
         <img id="blog-art" src={env.PUBLIC_STRAPI_HOST + data.json.data[0].attributes.blogArt.data?.attributes.url} alt="blog image">
+        </div>
     {/if}
     <div style="padding-bottom: 2rem">{@html data.content}</div>
     <hr style="border-top: 1px solid var(--fontColorBase);padding-bottom: 1rem ">
 </div>
 <style lang="scss">
+  .dynamic-aspect-ratio-box {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+  }
 
+  .dynamic-aspect-ratio-box img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover; // Use 'cover' or 'contain' depending on your design needs
+  }
     /*@use "$lib/styles/blogStyles.scss"*/
     .content-container {
       --content-width: 900px;
