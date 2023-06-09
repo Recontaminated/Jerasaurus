d7819d1f75f19facfa983182211b6a96d9a83647
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-06-08 20:39:08 -0700

    changed spacing

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,11 +3,20 @@
     import {onMount} from "svelte";
     import Prism from 'prismjs';
 
-    import { env } from '$env/dynamic/public';
+    import {env} from '$env/dynamic/public';
+
     export let data = {
         content: '',
         json: {
-            data: undefined
+            data: undefined,
+            blogArt: {
+                data: {
+                    attributes:{
+                        width: 0,
+                        height: 0
+                    }
+                }
+            }
         }
     };
     //call code on the browser
@@ -17,10 +26,9 @@
     })
 
     let artAR;
-   // this is so dumb
-    if (data.json.blogArt.data != null)
-    {
-       artAR = data.json.blogArt.data?.attributes.width / data.json.blogArt.data?.attributes.height *100;
+    // this is so dumb
+    if (data.json.blogArt.data != null) {
+        artAR = data.json.blogArt.data?.attributes.width / data.json.blogArt.data?.attributes.height * 100;
     }
 
 </script>
@@ -32,7 +40,7 @@
 </svelte:head>
 
 
-    <Header/>
+<Header/>
 <div class="content-container">
     <h1 class="blog-title">{data.json.title}</h1>
     <p id="pageDate">Updated: {(data.json.updated)} </p>
@@ -41,7 +49,7 @@
                 class="dynamic-aspect-ratio-box"
                 style={`padding-bottom: ${artAR}%`}
         >
-        <img id="blog-art" src={env.PUBLIC_STRAPI_HOST + data.json.blogArt.data?.attributes.url} alt="blog image">
+            <img id="blog-art" src={env.PUBLIC_STRAPI_HOST + data.json.blogArt.data?.attributes.url} alt="blog image">
         </div>
     {/if}
     <div style="padding-bottom: 2rem">{@html data.content}</div>
@@ -52,6 +60,7 @@
     position: relative;
     width: 100%;
     overflow: hidden;
+    margin-bottom: 1.6rem;
   }
 
   .dynamic-aspect-ratio-box img {
@@ -63,83 +72,89 @@
     object-fit: cover; // Use 'cover' or 'contain' depending on your design needs
   }
 
-    /*@use "$lib/styles/blogStyles.scss"*/
-    .content-container {
-      --content-width: 900px;
-      #blog-art {
-        width: 100%;
-        height: auto;
-        padding-bottom: 1rem;
-        //center it
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-      }
+  /*@use "$lib/styles/blogStyles.scss"*/
+  .content-container {
+    --content-width: 900px;
+
+    #blog-art {
+      width: 100%;
+      height: auto;
+      //center it
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+    }
 
     h1 {
-        font-size: 2.5rem;
-        font-weight: 600;
-        color: var(--fontColorBase);
-        margin-bottom: 1rem;
+      font-size: 2.5rem;
+      font-weight: 600;
+      color: var(--fontColorBase);
+      margin-bottom: 1rem;
     }
+
     #pageDate {
       font-family: apple-system, Inter, sans-serif, sans-serif;
       text-align: center;
-        font-size: 0.8rem;
-        font-weight: 200;
-        color: var(--fontColorBase);
-    }
-    .blog-title {
-      text-align: center;
-        font-family: Inter, sans-serif;
-        font-size: 3rem;
-        font-weight: 600;
-        margin-bottom: 0;
-    }
-    // we have to do this or else styles dont apply to the content
-    :global(body) {
-        color: var(--fontColorBase);
+      font-size: 0.8rem;
+      font-weight: 200;
+      color: var(--fontColorBase);
     }
 
-     :global(h1) {
-        font-family: Arial, Inter, sans-serif;
-        font-size: 2rem;
-        font-weight: 700;
-        padding-bottom: 0.7rem;
+    .blog-title {
+      text-align: center;
+      font-family: Inter, sans-serif;
+      font-size: 3rem;
+      font-weight: 600;
+      margin-bottom: 0;
+    }
+
+    // we have to do this or else styles dont apply to the content
+    :global(body) {
+      color: var(--fontColorBase);
+    }
+
+    :global(h1) {
+      font-family: Arial, Inter, sans-serif;
+      font-size: 2rem;
+      font-weight: 700;
+      margin-bottom: 0.7rem;
 
     }
 
     :global(h2) {
-        font-family: Arial, Inter, sans-serif;
-        font-size: 2rem;
-        font-weight: 600;
-        padding-bottom: 0.7rem;
+      font-family: Arial, Inter, sans-serif;
+      font-size: 2rem;
+      font-weight: 600;
+      margin-bottom: 0.7rem;
     }
 
     :global(p) {
-        padding-bottom: 0.7rem;
+      margin-bottom: 0.7rem;
 
     }
 
-      :global(strong) {
-        background-color: rgb(255, 178, 62);
-        background-image: linear-gradient(90deg, #c74677, #d65372, #e2616e, #ec716a, #f38168, #f99267, #fda468, #ffb56b);
-        background-size: 100%;
-        background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
-      :global(a) {
-        color: var(--accentColor);
-
-      }
-      :global(a:hover){
-        border-bottom: 1px solid var(--accentColor);
-      }
-      :global(img){
-       margin-left: auto;
-         margin-right: auto;
-        max-width: var(--content-width);
-      }
+    :global(strong) {
+      background-color: rgb(255, 178, 62);
+      background-image: linear-gradient(90deg, #c74677, #d65372, #e2616e, #ec716a, #f38168, #f99267, #fda468, #ffb56b);
+      background-size: 100%;
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
     }
+
+    :global(a) {
+      color: var(--accentColor);
+
+    }
+
+    :global(a:hover) {
+      border-bottom: 1px solid var(--accentColor);
+    }
+
+    :global(img) {
+      margin-left: auto;
+      margin-right: auto;
+      max-width: var(--content-width);
+    }
+  }
 
 </style>
